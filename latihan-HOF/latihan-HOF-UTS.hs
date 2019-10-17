80f8d85ed2a522808733bd9e1a5046ed8a48ad2c
lowell <lowell.sianipar@gmail.com>
2019-10-17 12:44:57 +0700

    latihan-UTS-Struktur Data Ekspresi-infix expression and evaluation

--- latihan-HOF/latihan-HOF-UTS.hs
+++ latihan-HOF/latihan-HOF-UTS.hs
@@ -1,18 +1,43 @@
 -- Struktur data ekspresi non infix
-data Expr   = C Float
-            | Add Expr Expr
-            | Sub Expr Expr
-            | Mul Expr Expr
-            | Div Expr Expr
+-- data Expr   = C Float
+--             | Add Expr Expr
+--             | Sub Expr Expr
+--             | Mul Expr Expr
+--             | Div Expr Expr
 
 -- evaluasi non infix
-evaluate :: Expr -> Float
-evaluate (C x)          = x
-evaluate (Add e1 e2)    = evaluate e1 + evaluate e2
-evaluate (Sub e1 e2)    = evaluate e1 - evaluate e2
-evaluate (Mul e1 e2)    = evaluate e1 * evaluate e2
-evaluate (Div e1 e2)    = evaluate e1 / evaluate e2
+-- evaluate :: Expr -> Float
+-- evaluate (C x)          = x
+-- evaluate (Add e1 e2)    = evaluate e1 + evaluate e2
+-- evaluate (Sub e1 e2)    = evaluate e1 - evaluate e2
+-- evaluate (Mul e1 e2)    = evaluate e1 * evaluate e2
+-- evaluate (Div e1 e2)    = evaluate e1 / evaluate e2
 
 -- misalkan
 -- evaluate (Sub (Mul (Add (C 10) (C 2)) (C 10)) (Div (C 1000) (C 10)))
+-- akan menghasilkan 20.0 karena ekivalen dengan ((10 + 2) * 10) - (1000 / 10)
+
+--
+--
+--
+--
+--
+
+-- Struktur data ekspresi infix
+data Expr   = C Float
+            | Expr :+ Expr
+            | Expr :- Expr
+            | Expr :* Expr
+            | Expr :/ Expr
+
+-- evaluasi struktur data ekspresi infix
+evaluate :: Expr -> Float
+evaluate (C x)         = x
+evaluate (e1 :+ e2)    = evaluate e1 + evaluate e2
+evaluate (e1 :- e2)    = evaluate e1 - evaluate e2
+evaluate (e1 :* e2)    = evaluate e1 * evaluate e2
+evaluate (e1 :/ e2)    = evaluate e1 / evaluate e2
+
+-- misalkan
+-- evaluate (((C 10 :+ C 2) :* C 10) :- (C 1000 :/ C 10))
 -- akan menghasilkan 20.0 karena ekivalen dengan ((10 + 2) * 10) - (1000 / 10)
