5f7d188974f5846eea297067c486b3be4239387f
lowell <lowell.sianipar@gmail.com>
2019-10-17 18:21:51 +0700

    latihan-UTS-Struktur Data Ekspresi-eksperimen map

--- latihan-HOF/latihan-HOF-UTS.hs
+++ latihan-HOF/latihan-HOF-UTS.hs
@@ -29,6 +29,7 @@ data Expr   = C Float
             | Expr :- Expr
             | Expr :* Expr
             | Expr :/ Expr
+        deriving (Show)
 
 -- evaluasi struktur data ekspresi infix
 evaluate :: Expr -> Float
@@ -41,3 +42,20 @@ evaluate (e1 :/ e2)    = evaluate e1 / evaluate e2
 -- misalkan
 -- evaluate (((C 10 :+ C 2) :* C 10) :- (C 1000 :/ C 10))
 -- akan menghasilkan 20.0 karena ekivalen dengan ((10 + 2) * 10) - (1000 / 10)
+
+--
+--
+--
+--
+--
+
+mapExpr                 :: (Float -> Float) -> Expr -> Expr
+mapExpr f (C x)         = C (f x)
+mapExpr f (e1 :+ e2)    = (mapExpr f e1) :+ (mapExpr f e2)
+mapExpr f (e1 :- e2)    = (mapExpr f e1) :- (mapExpr f e2)
+mapExpr f (e1 :* e2)    = (mapExpr f e1) :* (mapExpr f e2)
+mapExpr f (e1 :/ e2)    = (mapExpr f e1) :/ (mapExpr f e2)
+
+-- sehingga bila dijalankan,
+-- mapExpr (+3) (C 10 :+ C 12)
+-- maka ekspresi menjadi C 13.0 :+ C 15.0
