cd75f0cea3f458e9f4b7454282812bafa6782bea
lowell <lowell.sianipar@gmail.com>
2019-10-18 23:17:22 +0700

    latihan-mergesort-#3

--- latihan-HOF/latihan-MergeSort.hs
+++ latihan-HOF/latihan-MergeSort.hs
@@ -3,3 +3,8 @@ merge arrx []                            = arrx
 merge [] arry                            = arry
 merge arrx@(x:xs) arry@(y:ys) | (x <= y) = x : merge xs arry
                               | (x > y)  = y : merge arrx ys
+
+mergeSort :: [Int] -> [Int]
+mergeSort []  = []
+mergeSort [x] = [x]
+mergeSort xs  = merge (mergeSort(take (div (length xs) 2) xs)) (mergeSort(drop (div (length xs) 2) xs))
