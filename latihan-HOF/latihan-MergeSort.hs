3c6c4f2e7c2f434da687b4f832cb370294b509d5
lowell <lowell.sianipar@gmail.com>
2019-10-18 23:09:43 +0700

    latihan-mergesort-#2

--- latihan-HOF/latihan-MergeSort.hs
+++ latihan-HOF/latihan-MergeSort.hs
@@ -1,3 +1,5 @@
 merge :: [Int] -> [Int] -> [Int]
+merge arrx []                            = arrx
+merge [] arry                            = arry
 merge arrx@(x:xs) arry@(y:ys) | (x <= y) = x : merge xs arry
                               | (x > y)  = y : merge arrx ys
