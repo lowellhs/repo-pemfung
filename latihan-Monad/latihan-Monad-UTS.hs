11eb5c9ce5a51e44bb0947e0553e3cfa7af64a23
lowell <lowell.sianipar@gmail.com>
2019-10-19 10:51:32 +0700

    latihan-UTS-Monad-forloop#2

--- latihan-Monad/latihan-Monad-UTS.hs
+++ latihan-Monad/latihan-Monad-UTS.hs
@@ -39,5 +39,5 @@
 -- ekivalen dengan print 1 >> print 2 >> print 3 >> print 4
 
 -- FORLOOP --
-forloop []     action = return ()
-forloop (x:xs) action = (action x) >> (forloop xs action)
+-- forloop []     action = return ()
+-- forloop (x:xs) action = (action x) >> (forloop xs action)
